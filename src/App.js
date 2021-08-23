f34d65cc0328282723bf2ae9e0d0252860c8844e
deanchen <antlet2004@gmail.com>
2021-08-23 15:58:25 +0800

    chore: install router, loglevel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import React from "react";
 import logo from './logo.svg';
 import loading from "./images/loading.png";
+import defaultImage from "./images/default.png";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
+import log from "loglevel";
 
-function App() {
-  return (
+
+function LandingPage(){
+  const history = useHistory();
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      log.debug("load finished");
+      history.push("/default");
+    }, 1000);
+  }, []);
+  
+  return(
     <div className="App">
       <img src={loading}   />
     </div>
+  );
+}
+
+function DefaultPage(){
+  return (
+    <div className="App">
+      <img src={defaultImage}   />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/default" >
+          <DefaultPage />
+        </Route>
+        <Route path="/" >
+          <LandingPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
