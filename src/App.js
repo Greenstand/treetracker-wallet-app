9cef0194d2a87e674de3b03d6216e4e4ef3d8c88
deanchen <antlet2004@gmail.com>
2021-08-24 14:06:26 +0800

    chore: wrong with history.action

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from './logo.svg';
 import loading from "./images/loading.png";
 import defaultImage from "./images/default.png";
+import detailsImage from "./images/details.png";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, useHistory, useLocation } from "react-router-dom";
 import log from "loglevel";
@@ -30,9 +31,30 @@ function LandingPage(){
 }
 
 function DefaultPage(){
+  const history = useHistory();
+  log.debug("history action:", history.action);
+  function handleClick(){
+      log.debug("load finished");
+      history.push("/details");
+  };
+  
   return (
-    <div className="App">
+    <div className="App" onClick={handleClick} >
       <img src={defaultImage} />
+    </div>
+  );
+}
+
+function DetailsPage(){
+  const history = useHistory();
+  function handleClick(){
+      log.debug("load finished");
+      history.go(-1);
+  };
+  
+  return (
+    <div className="App" onClick={handleClick} >
+      <img src={detailsImage} />
     </div>
   );
 }
@@ -45,17 +67,22 @@ function DefaultPage(){
 function Test(){
   const location = useLocation();
   log.debug("location:", location);
+  const history = useHistory();
+  log.debug("!history action:", history.action);
 
   return (
     <TransitionGroup>
       <CSSTransition
         key={location.key}
-        classNames="fade"
-        timeout={300}
+        classNames={`fade-${history.action.toLowerCase()}`}
+        timeout={8000}
       >
         <Switch location={location} >
           <Route path="/default" >
             <DefaultPage />
+          </Route>
+          <Route path="/details" >
+            <DetailsPage />
           </Route>
           <Route path="/home" >
             <LandingPage />
