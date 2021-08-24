c34dc9da391a97404e6a3ad09205d7e2fec18348
deanchen <antlet2004@gmail.com>
2021-08-24 11:37:51 +0800

    feat: page router transition

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import logo from './logo.svg';
 import loading from "./images/loading.png";
 import defaultImage from "./images/default.png";
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, useHistory, useLocation } from "react-router-dom";
 import log from "loglevel";
+import {   TransitionGroup,CSSTransition } from "react-transition-group";
 
 
 function LandingPage(){
   const history = useHistory();
 
-  React.useEffect(() => {
-    setTimeout(() => {
+//  React.useEffect(() => {
+//    setTimeout(() => {
+//      log.debug("load finished");
+//      history.push("/default");
+//    }, 1000);
+//  }, []);
+  function handleClick(){
       log.debug("load finished");
       history.push("/default");
-    }, 1000);
-  }, []);
+  };
   
   return(
-    <div className="App">
-      <img src={loading}   />
+    <div className="App" onClick={handleClick} >
+      <img src={loading} />
     </div>
   );
 }
@@ -27,22 +32,45 @@ function LandingPage(){
 function DefaultPage(){
   return (
     <div className="App">
-      <img src={defaultImage}   />
+      <img src={defaultImage} />
     </div>
   );
 }
 
+//const routes = [
+//  { path: '/', name: 'Home', Component: LandingPage },
+//  { path: '/default', name: 'Default', Component: DefaultPage },
+//]
+
+function Test(){
+  const location = useLocation();
+  log.debug("location:", location);
+
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        key={location.key}
+        classNames="fade"
+        timeout={300}
+      >
+        <Switch location={location} >
+          <Route path="/default" >
+            <DefaultPage />
+          </Route>
+          <Route path="/home" >
+            <LandingPage />
+          </Route>
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
+
 function App() {
+
   return (
     <Router>
-      <Switch>
-        <Route path="/default" >
-          <DefaultPage />
-        </Route>
-        <Route path="/" >
-          <LandingPage />
-        </Route>
-      </Switch>
+      <Test/>
     </Router>
   );
 }
