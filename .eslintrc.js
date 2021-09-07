6d5f29fd634980af11bcb8d83cbca073fdb0e049
VWRoli <fustroli@gmail.com>
2021-09-07 13:45:11 +0200

    chore: ISSUE #50 Solve the eslint for cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,37 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-        jest: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:cypress/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-    ],
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'cypress'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': ['warn'],
+    'react/display-name': ['warn'],
+    'cypress/no-unnecessary-waiting': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    parser: "babel-eslint",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2020,
-        sourceType: "module",
-    },
-    plugins: ["react"],
-    rules: {
-        "react/prop-types": "off",
-        "no-unused-vars": ["warn"],
-        "react/display-name": ["warn"],
-    },
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
+  },
 };
