5da452a8fe2a91b47dec7b627ec0ae12b0c46d3c
deanchen <antlet2004@gmail.com>
2021-09-29 15:54:06 +0800

    fix: broken cypress component tool

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,5 +4,7 @@ module.exports = (on, config) => {
   //copy env
   config.env = { ...config.env, ...process.env };
 
+  require('@cypress/react/plugins/react-scripts')(on, config);
+
   return config;
 };
