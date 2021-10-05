4fa5c916003a5d68e474bbb490363182fa8246a8
Roland Füst <31432331+VWRoli@users.noreply.github.com>
2021-10-05 04:23:16 +0200

    feat: issue #124 image thumbnail (#134)

---
+++ src/components/OptimizedImage.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export default function OptimizedImage(props) {
+  const {
+    src,
+    width = 320,
+    height,
+    quality,
+    screenWidths = [1600, 1280, 960, 0],
+    imageSizes = [400, 300, 250, 200],
+    fixed,
+    rotation,
+    ...rest
+  } = props;
+
+  if (!src) return <></>;
+
+  const cdnPath = 'https://cdn.statically.io/img';
+  const matches = src.match(/\/\/(.*?)\/(.*)/);
+
+  let cdnUrl, sizes, srcSet;
+
+  if (matches?.length > 1) {
+    const domain = matches[1];
+    const imagePath = matches[2];
+    const params =
+      `f=auto,w=${width}` +
+      (height ? `,h=${height}` : '') +
+      (quality ? `,q=${quality}` : '');
+
+    cdnUrl = `${cdnPath}/${domain}/${params}/${imagePath}`;
+
+    if (!fixed && screenWidths.length === imageSizes.length) {
+      sizes = screenWidths
+        .map((size, i) => `(min-width: ${size}px) ${imageSizes[i]}px`)
+        .join(', ');
+      srcSet = imageSizes
+        .map((size) => `${cdnPath}/${domain}/${params}/${imagePath} ${size}w`)
+        .join(', ');
+    }
+  }
+
+  return (
+    <>
+      <img
+        src={cdnUrl || src}
+        alt=".."
+        srcSet={srcSet}
+        sizes={sizes}
+        loading="lazy"
+        style={{
+          inset: 0,
+          objectFit: 'cover',
+          width: 'calc(100% + 2px)', // width for image rotation
+          height: 'calc(100% + 2px)', // height for image rotation
+          transform: `rotate(${rotation}deg)`,
+          borderRadius: '8px',
+        }}
+        {...rest}
+      />
+    </>
+  );
+}
