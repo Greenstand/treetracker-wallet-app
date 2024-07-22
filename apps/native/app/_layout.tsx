207fc02c64f5deef8f21f6924f0dd85abbaef2d6
Stanley Pierre Louis <stanleypl75@gmail.com>
2024-07-21 19:31:59 -0500

    feat: add splash screen

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,8 +1,18 @@
-import { Stack } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
 import {
   MD3LightTheme as DefaultTheme,
   PaperProvider,
 } from "react-native-paper";
+
+export {
+  ErrorBoundary,
+} from 'expo-router';
+
 
 const theme = {
   ...DefaultTheme,
@@ -13,7 +23,36 @@ const theme = {
   },
 };
 
+
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+
   return (
     <PaperProvider theme={theme}>
       <Stack>
