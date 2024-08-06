fba2c8f3f376fd0441b154a0e82feba2a6659def
dagmawig <dgebreselasse@gmail.com>
2024-08-05 22:46:40 -0500

    feat: support multiple notify channels, add notification promise

--- __tests__/e2e/message.spec.js
+++ __tests__/e2e/message.spec.js
@@ -10,82 +10,86 @@ describe('subscribes clients to postgres channel "new_tree_notification" and add
     })
 
     // creates two clients and confirms their subscription to a postgres channel
-    it('subscribes two cilents', async () => {
+    it('subscribes two cilents', (done) => {
 
         //  tree object to be added to table
-        const treeObj = {
-            latest_capture_id: uuid.v4(),
-            age: 1
+        const messageObj = {
+            name: 'tree',
+            data: {
+                capture_id: uuid.v4()
+            }
         }
 
         // query to create notification function on postgres schema
-        let funQueryRes;
-        try {
-            funQueryRes = await pgClient.query(`CREATE OR REPLACE FUNCTION queue.new_tree_notify_trigger() RETURNS TRIGGER AS $$
+        pgClient.query(`CREATE OR REPLACE FUNCTION queue.new_message_notify_trigger() RETURNS TRIGGER AS $$
             DECLARE
             BEGIN
-                PERFORM pg_notify(cast('insert_notification' as text), row_to_json(new)::text);
-                RETURN NEW;
+                CASE NEW.name
+                    WHEN 'tree' THEN
+                        PERFORM pg_notify(cast('tree_notification' as text), row_to_json(new)::text);
+                        RETURN NEW;
+                    WHEN 'planter' THEN
+                        PERFORM pg_notify(cast('planter_notification' as text), row_to_json(new)::text);
+                        RETURN NEW;
+                    ELSE
+                        RETURN NEW;
+                END CASE;
             END;
             $$ LANGUAGE plpgsql;`
-            );
-
-        } catch (err) {
-            throw Error(`posgtres function creation error: ${err}`);
-        }
-
-
-        console.log("function created: ", funQueryRes)
+            , (err, res) => {
+                if (err) throw Error(`function creation error: ${err}`)
+                console.log("function created: ", res);
+            });
 
         // query to create trigger on postgres table
-        let triggerQueryRes;
-        try {
-            await pgClient.query(`DROP TRIGGER IF EXISTS new_tree_insert_trigger on queue.tree`)
-            triggerQueryRes = await pgClient.query(`CREATE TRIGGER new_tree_insert_trigger BEFORE INSERT ON queue.tree
-            FOR EACH ROW EXECUTE PROCEDURE queue.new_tree_notify_trigger();`)
-        } catch (err) {
-            throw Error(`postgres trigger drop/creation error: ${err}`);
-        }
-
-        console.log("trigger created: ", triggerQueryRes);
-
-        // define action during notification
-        pgClient.on('notification', msg => {
-            const newRow = JSON.parse(msg.payload);
-            expect(newRow).toMatchObject(treeObj); // eslint-disable-line
+        pgClient.query(`DROP TRIGGER IF EXISTS new_message_insert_trigger on queue.message`, (err) => {
+            if (err) throw Error(`trigger drop error: ${err}`);
+        })
+        pgClient.query(`CREATE TRIGGER new_message_insert_trigger BEFORE INSERT ON queue.message
+            FOR EACH ROW EXECUTE PROCEDURE queue.new_message_notify_trigger();`, (err, res) => {
+            if (err) throw Error(`trigger creation error: ${err}`);
+            console.log("trigger created: ", res);
         });
 
-        pgClient2.on('notification', msg => {
-            const newRow = JSON.parse(msg.payload);
-            expect(newRow).toMatchObject(treeObj); // eslint-disable-line
+        // create promises to receive notifications and verify message/payload
+        Promise.all([
+            new Promise((resolve) => {
+                pgClient.on('notification', msg => {
+                    const newRow = JSON.parse(msg.payload);
+                    return resolve(newRow);
+                });
+            }),
+            new Promise((resolve) => {
+                pgClient2.on('notification', msg => {
+                    const newRow = JSON.parse(msg.payload);
+                    return resolve(newRow);
+                });
+            })
+        ]).then(values => {
+            expect(values[0]).toMatchObject(messageObj); // eslint-disable-line
+            expect(values[1]).toMatchObject(messageObj); // eslint-disable-line
+            done();
         });
 
         // subscribe clients to a postgres channel
-        let subscribeQueryRes;
-        try {
-            subscribeQueryRes = await pgClient.query(`LISTEN insert_notification`);
-        } catch (err) {
-            throw Error(`postgres channel subscription error: ${err}`);
-        }
-        console.log("listen notification success: ", subscribeQueryRes);
-        let subscribeQueryRes2;
-        try {
-            subscribeQueryRes2 = await pgClient2.query(`LISTEN insert_notification`);
-        } catch (err) {
-            throw Error(`postgres channel subscription error: ${err}`);
-        }
-        console.log("listen notification success: ", subscribeQueryRes2);
+        pgClient.query(`LISTEN tree_notification`, (err, res) => {
+            if (err) throw Error(`subscription error: ${err}`);
+            console.log("listen notification success: ", res);
+        });
 
-        // insert new row to postgres table
-        const text = `INSERT into queue.tree(latest_capture_id, age) values ($1, $2) RETURNING *`;
-        const values = [treeObj.latest_capture_id, treeObj.age];
-        let insertQueryRes
-        try {
-            insertQueryRes = await pgClient.query(text, values);
-        } catch (err) {
-            throw Error(`postgres row insertion error: ${err}`);
-        }
-        console.log("postgres row insertion success: ", insertQueryRes);
+        pgClient2.query(`LISTEN tree_notification`, (err, res) => {
+            if (err) throw Error(`subscription error: ${err}`);
+            console.log("listen notification success: ", res);
+        });
+
+        // insert new row to postgres table, serves same function as sending a message
+        const text = `INSERT into queue.message(name, data) values ($1, $2) RETURNING *`;
+        const values = [messageObj.name, messageObj.data];
+
+        pgClient.query(text, values, (err, res) => {
+            if (err) throw Error(`insertion error: ${err}`);
+            console.log("postgres row insertion success: ", res);
+        });
 
     }, 30000)
 })
