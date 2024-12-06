214ddc0a8c29ddb38fca84179696d318c19ab728
calliel.freitas <callieldefreitas@gmail.com>
2024-12-05 23:09:12 -0300

    feat: adding login button and stylish

--- apps/native/screens/auth/Login.screen.tsx
+++ apps/native/screens/auth/Login.screen.tsx
@@ -1,15 +1,18 @@
-import { Link } from "expo-router";
 import React from "react";
 import {
-  Dimensions,
-  Platform,
-  Text,
   View,
+  Text,
   StyleSheet,
+  Dimensions,
+  SafeAreaView,
+  TouchableOpacity,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
   KeyboardAvoidingView,
+  Platform,
   TextInput,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import TreeTraderLogo from "@/assets/svg/TreeTraderLogo.svg";
 
 const { height, width } = Dimensions.get("window");
 
@@ -28,6 +31,7 @@ const DATA: LoginTypes[] = [
 const LoginScreen = () => {
   const [login, onChangeLogin] = React.useState("");
   const [password, onChangePassword] = React.useState("");
+  const loginButtonEnabled = login.length > 0 && password.length > 0;
 
   return (
     <SafeAreaView style={styles.loginPageContainer}>
@@ -35,7 +39,7 @@ const LoginScreen = () => {
         behavior={Platform.OS === "ios" ? "padding" : "height"}>
         {/* TOP */}
         <View style={styles.logoContainer}>
-          <Text style={{ color: "white" }}>LOGOTIPO TRADE WALLET</Text>
+          <TreeTraderLogo />
         </View>
         <View style={styles.textTitlePageContainer}>
           <Text style={styles.textTitleLoginPage}>Log In</Text>
@@ -47,25 +51,61 @@ const LoginScreen = () => {
         {/* MIDDLE */}
         <View style={styles.inputBoxContainer}>
           <View style={styles.inputBox}>
+            <Text
+              style={[
+                styles.placeholderInputBox,
+                login ? styles.placeholderActive : null,
+              ]}>
+              Email
+            </Text>
             <TextInput
-              style={styles.placeholderInputBox}
+              style={styles.textInputBox}
               onChangeText={onChangeLogin}
               value={login}
-              placeholder="Email"
+              inputMode="email"
             />
+            <View style={styles.underline} />
           </View>
         </View>
 
         <View style={styles.inputBoxContainer}>
           <View style={styles.inputBox}>
+            <Text
+              style={[
+                styles.placeholderInputBox,
+                password ? styles.placeholderActive : null,
+              ]}>
+              Password
+            </Text>
             <TextInput
-              style={styles.placeholderInputBox}
+              style={styles.textInputBox}
               onChangeText={onChangePassword}
               value={password}
-              placeholder="Senha"
               secureTextEntry={true}
+              inputMode="text"
             />
+            <View style={styles.underline} />
           </View>
+        </View>
+
+        {/* Login Button */}
+        <View style={styles.loginButtonContainer}>
+          <TouchableOpacity
+            style={
+              loginButtonEnabled
+                ? styles.loginButtonAvailable
+                : styles.loginButtonUnavailable
+            }
+            onPress={() => {}}>
+            <Text
+              style={
+                loginButtonEnabled
+                  ? styles.placeholderLoginButtonAvailable
+                  : styles.placeholderLoginButtonUnavailable
+              }>
+              LOG IN
+            </Text>
+          </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -84,52 +124,108 @@ const styles = StyleSheet.create({
   logoContainer: {
     // container
     alignContent: "center",
-    height: "25%",
+    height: "20%",
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "green",
+    backgroundColor: "white",
   },
   textTitlePageContainer: {
     // container
-    marginTop: 10,
-    backgroundColor: "yellow",
+    height: "15%",
+    marginTop: 0,
+    alignItems: "center",
+    backgroundColor: "white",
   },
   textTitleLoginPage: {
+    width: "90%",
     fontSize: 30,
     fontWeight: "400",
     color: "black",
-    marginLeft: 10,
+    backgroundColor: "white",
   },
   descriptionLogin: {
+    width: "90%",
     marginTop: 5,
     fontSize: 15,
     fontWeight: "400",
     color: "black",
-    marginLeft: 10,
   },
   // ------------------------------------------------------------ MIDDLE
   inputBoxContainer: {
     // container
-    height: "15%",
-    marginTop: 10,
+    height: "12%",
+    marginTop: 20,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red",
-  },
-  inputBox: {
-    height: "80%",
-    width: "90%",
-
     backgroundColor: "white",
   },
-  placeholderInputBox: {},
+  inputBox: {
+    height: "100%",
+    width: "90%",
+    backgroundColor: "#f1f3ef",
+  },
+  placeholderInputBox: {
+    position: "absolute",
+    top: 16,
+    left: 5,
+    fontSize: 20,
+    color: "#aaa",
+  },
+  placeholderActive: {
+    fontSize: 12,
+    top: 5,
+    color: "#666",
+  },
+  textInputBox: {
+    height: "100%",
+    fontSize: 20,
+  },
+  underline: {
+    position: "absolute",
+    bottom: 0, // Linha na parte inferior
+    left: 0,
+    right: 0,
+    height: 2,
+    opacity: 0.42,
+    backgroundColor: "black",
+  },
   loginButtonContainer: {
     // container
+    backgroundColor: "white",
+    height: "15%",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  loginButtonUnavailable: {},
-  loginButtonOk: {},
-  placeholderLoginButton: {},
+  loginButtonUnavailable: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "gray",
+    opacity: 0.2,
+    height: "50%",
+    width: "91%",
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  loginButtonAvailable: {
+    backgroundColor: "#61892F",
+    marginTop: 10,
+    height: "50%",
+    width: "91%",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  placeholderLoginButtonAvailable: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "500",
+  },
+  placeholderLoginButtonUnavailable: {
+    fontSize: 20,
+    color: "gray",
+    fontWeight: "500",
+  },
   // ---------------------------------------------------------- BOTTOM
   forgotPasswordContainer: {
     // container
