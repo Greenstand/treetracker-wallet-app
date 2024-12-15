8da8bfc5b2cc244bc6b45ee514f6ed79b73eb73b
kshitij79 <kshitijpathania@gmail.com>
2024-12-15 18:47:39 -0500

    feat: add bottom navigation bar for home, send, and settings pages
    Signed-off-by: kshitij79 <kshitijpathania@gmail.com>

---
+++ apps/web/src/app/send/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+export default function Send() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}>
+      <Typography variant="h4">Send Page</Typography>
+    </Box>
+  );
+}
