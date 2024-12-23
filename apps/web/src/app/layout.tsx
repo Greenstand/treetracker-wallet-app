8c832f75c7f890a64f978432342bbd143b24786e
Kshitij Pathania <kshitijpathania@gmail.com>
2024-12-22 21:44:04 -0500

    fix: remove Header from Login and Signup pages (#295) (#308)
    Signed-off-by: kshitij79 <kshitijpathania@gmail.com>

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
@@ -6,17 +8,25 @@ import theme from "@/theme";
 import { Container } from "@mui/material";
 import Header from "@/components/header/Header";
 import BottomNavigationBar from "@/components/navigation/BottomNavigatorBar";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const authRoutes = ["/login", "/signup"];
+
+  // Check if the current route is an authentication page
+  const isAuthPage = authRoutes.includes(pathname ?? "");
+
   return (
     <html lang="en">
       <body>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Header />
+            {!isAuthPage && <Header />}
             <Container maxWidth="sm">{props.children}</Container>
-            <BottomNavigationBar />
+            {!isAuthPage && <BottomNavigationBar />}
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
