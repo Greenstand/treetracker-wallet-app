b25b54534cec4eacdf29854033fb8f989648b5ca
Stanley  Pierre-Louis <stanleypl75@gmail.com>
2025-02-02 16:27:52 -0500

    chore: change next.config.mjs to next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['wallet_state'],
+  transpilePackages: ["wallet_state"],
 };
 module.exports = nextConfig;
