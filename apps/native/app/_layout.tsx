687bdeb04423ced415156c1224d99ade0ed51fbe
Stanley  Pierre-Louis <stanleypl75@gmail.com>
2025-02-19 23:12:52 -0500

    refactor: rename function, add slot

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,32 +1,14 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import "react-native-reanimated";
-import {
-  MD3LightTheme as DefaultTheme,
-  PaperProvider,
-} from "react-native-paper";
-
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native";
-
-export { ErrorBoundary } from "expo-router";
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: "tomato",
-    secondary: "yellow",
-  },
-};
 
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
+export default function AppLayout() {
+  const [areFontsLoaded, fontLoadError] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     Roboto: require("../assets/fonts/Roboto-Regular.ttf"),
     RobotoBold: require("../assets/fonts/Roboto-Bold.ttf"),
@@ -35,48 +17,10 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
+    if (areFontsLoaded || fontLoadError) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [areFontsLoaded, fontLoadError]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <RootLayoutNav />
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+  return <Slot />;
 }
-
-export const options = {
-  headerShown: false,
-};
-
-function RootLayoutNav() {
-  return (
-    <PaperProvider theme={theme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={options} />
-        <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)/register" options={{ headerShown: false }} />
-        <Stack.Screen name="accountConfirmation" options={options} />
-      </Stack>
-    </PaperProvider>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
