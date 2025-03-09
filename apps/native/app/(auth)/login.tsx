a610a62afcde392f23008b3fc8d37e2b2b6ff1bf
Miracle <anajemiracle@gmail.com>
2025-03-09 17:12:39 +0100

    feat: add reset password screen

--- apps/native/app/(auth)/login.tsx
+++ apps/native/app/(auth)/login.tsx
@@ -127,7 +127,7 @@ const styles = StyleSheet.create({
   commonText: {
     textAlign: "center",
     color: "#222629DE",
-    fontSize: 19,
+    fontSize: 16,
   },
   resetText: {
     color: "#61892F",
