f899fb5cf9cf102e4eb343872f8e154c5106edf0
Amith <amithpremnagandla@gmail.com>
2025-04-04 17:23:18 -0400

    feat(infrastructure): add terraform configs for aws cloudfront/s3 static website deployment

---
+++ terraform/wallet-infra/dev.tfvars
@@ -0,0 +1,5 @@
+# dev.tfvars
+aws_region  = "us-east-1"
+bucket_name = "wallet-dev-treetracker"
+domain_name = ""
+environment = "dev"
---
+++ terraform/wallet-infra/provider.tf
@@ -0,0 +1,13 @@
+# provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
