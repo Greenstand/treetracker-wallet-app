9e4687f261d8f20359cc4db6ca14e9fb3377c168
kshitij79 <kshitijpathania@gmail.com>
2025-04-06 16:24:07 -0400

    feat: implemented wallet page
    Signed-off-by: kshitij79 <kshitijpathania@gmail.com>

---
+++ apps/web/cypress/component/WalletItem.cy.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import WalletItem from "../../src/app/wallet/WalletItem";
+
+describe("WalletItem Component", () => {
+  it("displays wallet name and amount correctly", () => {
+    cy.mount(
+      <WalletItem name="Savings Wallet" amount={1000} createdAt="2023-04-15" />,
+    );
+    cy.contains("Savings Wallet").should("exist");
+    cy.contains("1000").should("exist");
+  });
+
+  it("formats date correctly", () => {
+    cy.mount(
+      <WalletItem name="Travel Wallet" amount={500} createdAt="Apr 15, 2023" />,
+    );
+    cy.contains("Travel Wallet").should("exist");
+    cy.contains("Apr 15, 2023").should("exist");
+  });
+
+  it("handles zero amount correctly", () => {
+    cy.mount(
+      <WalletItem name="Empty Wallet" amount={0} createdAt="2023-04-15" />,
+    );
+    cy.contains("Empty Wallet").should("exist");
+    cy.contains("0").should("exist");
+  });
+});
