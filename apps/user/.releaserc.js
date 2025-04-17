9429be0d938afb8062ddf8a3a77721f51d6b86ab
Amith <amithpremnagandla@gmail.com>
2025-04-17 15:12:13 -0400

    fix: update semantic-release config to properly create tags

---
+++ apps/user/.releaserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  branches: ["main"],
+  tagFormat: "user-v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    ["@semantic-release/github", {}],
+  ],
+};
