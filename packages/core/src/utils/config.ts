03ff9fdfc57ffe8ad882dffa8f51eb13b39aa3c2
stanley <stanleypl75@gmail.com>
2025-07-18 09:33:06 -0400

    fix: cross platform api config

--- packages/core/src/utils/config.ts
+++ packages/core/src/utils/config.ts
@@ -1,9 +1,16 @@
-let config;
+let WALLET_APP_API: string;
 
-if (typeof window === "undefined") {
-  config = require("./config.web");
+const isNative =
+  typeof navigator !== "undefined" && navigator.product === "ReactNative";
+
+if (!isNative) {
+  WALLET_APP_API = process.env.NEXT_PUBLIC_WALLET_APP_API ?? WALLET_APP_API;
 } else {
-  config = require("./config.native");
+  const Constants = require("expo-constants").default;
+  WALLET_APP_API =
+    Constants.expoConfig?.extra?.apiBaseUrl ??
+    Constants.manifest?.extra?.apiBaseUrl ??
+    WALLET_APP_API;
 }
 
-export const WALLET_APP_API = config.WALLET_APP_API;
+export { WALLET_APP_API };
