92cdb9caad701c5bc9206c5f0077401e56ec77ce
Daiyin Yu <yudaiyin0101@gmail.com>
2025-07-23 16:01:39 -0700

    feat: implement welcome screen and connect it to login flow

--- apps/native/babel.config.js
+++ apps/native/babel.config.js
@@ -2,10 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
-    env: {
-      production: {
-        plugins: ["react-native-paper/babel", "react-native-reanimated/plugin"],
-      },
-    },
+    plugins: ["react-native-paper/babel", "react-native-reanimated/plugin"],
   };
 };
