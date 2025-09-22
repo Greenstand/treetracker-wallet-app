4180eb437af746b0d6fdb621821ace6af61d7c43
jenniferteaa <teancyjennifer05@gmail.com>
2025-09-22 01:49:27 -0500

    Switched dotenv loading from CJS to ESM

--- packages/queue/pgClient.js
+++ packages/queue/pgClient.js
@@ -1,21 +1,21 @@
 const { Client } = require("pg");
 const log = require("loglevel");
-require("dotenv").config();
-// require("dotenv").config({ path: "../../apps/user/.dev.env" });
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
-//console.log(connectionString);
+// console.log("This is the DB link: ", connectionString);
 if (!connectionString) {
   log.warn("DATABASE_URL is not set!");
 }
 
-const pgClient = new Client({
+export const pgClient = new Client({
   connectionString,
 });
 
-const pgClient2 = new Client({
-  connectionString,
-});
+// const pgClient2 = new Client({
+//   connectionString,
+// });
 
 pgClient.connect(err => {
   if (err) {
@@ -27,14 +27,15 @@ pgClient.connect(err => {
   }
 });
 
-pgClient2.connect(err => {
-  if (err) {
-    const message = `error in connecting to DB: ${err}`;
-    log.error(message);
-    throw message;
-  } else {
-    log.info(`Database connected!`);
-  }
-});
+// pgClient2.connect(err => {
+//   if (err) {
+//     const message = `error in connecting to DB: ${err}`;
+//     log.error(message);
+//     throw message;
+//   } else {
+//     log.info(`Database connected!`);
+//   }
+// });
 
-module.exports = { pgClient, pgClient2 };
+// module.exports = { pgClient, pgClient2 };
+// module.exports = { pgClient };
