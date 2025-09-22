e5ed852153078863c9bb11e15bdd48a2cf6a8228
jenniferteaa <teancyjennifer05@gmail.com>
2025-09-22 01:40:44 -0500

    Integrate fetchTokenFromKeycloak with caching

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,14 +1,13 @@
-import { Module } from "@nestjs/common";
-import * as dotenv from "dotenv";
-import { UserModule } from "./user/user.module";
-import { UserController } from "./user/user.controller";
-import { UserService } from "./user/user.service";
 import { HttpModule } from "@nestjs/axios";
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { QueueListenerService } from "./queue-listener/queue-listener.service";
-import { QueueModule } from "./queue-listener/queue-listener.module";
+import * as dotenv from "dotenv";
 import { AuthModule } from "./auth/auth.module";
 import { AuthService } from "./auth/auth.service";
+import { QueueModule } from "./queue-listener/queue-listener.module";
+import { UserController } from "./user/user.controller";
+import { UserModule } from "./user/user.module";
+import { UserService } from "./user/user.service";
 
 dotenv.config();
 
