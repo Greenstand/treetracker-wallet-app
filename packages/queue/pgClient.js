ebeede5de63d7e3e13cbdedd070e7a9f8a14ecf7
jenniferteaa <teancyjennifer05@gmail.com>
2025-09-22 01:49:49 -0500

    Removed seemed to be duplicates

--- packages/queue/pgClient.js
+++ packages/queue/pgClient.js
@@ -12,11 +12,6 @@ if (!connectionString) {
 export const pgClient = new Client({
   connectionString,
 });
-
-// const pgClient2 = new Client({
-//   connectionString,
-// });
-
 pgClient.connect(err => {
   if (err) {
     const message = `error in connecting to DB: ${err}`;
@@ -26,16 +21,3 @@ pgClient.connect(err => {
     log.info(`Database connected!`);
   }
 });
-
-// pgClient2.connect(err => {
-//   if (err) {
-//     const message = `error in connecting to DB: ${err}`;
-//     log.error(message);
-//     throw message;
-//   } else {
-//     log.info(`Database connected!`);
-//   }
-// });
-
-// module.exports = { pgClient, pgClient2 };
-// module.exports = { pgClient };
