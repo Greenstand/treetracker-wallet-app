63f711a6e8136c5df5e3f08f1a12f6d52265895e
Stanley  Pierre-Louis <stanleypl75@gmail.com>
2025-10-16 00:28:29 -0400

    chore(native): remove metro configuration

--- apps/native/index.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from 'expo';
-import { ExpoRoot } from 'expo-router';
-
-export function App() {
-  const ctx = require.context('./app');
-  return <ExpoRoot context={ctx} />;
-}
-
-registerRootComponent(App);
