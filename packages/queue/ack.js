e30817f017c1934c842eaa84411e9e2ad2fd95d6
keyurparashtekar <p.parashtekar@gmail.com>
2026-08-03 18:41:23 -0500

    refactor(queue): use parameterized queries in ack.js

--- packages/queue/ack.js
+++ packages/queue/ack.js
@@ -1,13 +1,15 @@
 // sends message receipt confirmation
 async function ack({ pgClient, id, dateStr, clientID }) {
-  const sql = `UPDATE queue.message set ack=jsonb_set(ack, '{${clientID}}', '"${dateStr}"') where id='${id}'`;
-  const sql2 = `SELECT * from queue.message where id='${id}'`;
+  const sql = `UPDATE queue.message set ack=jsonb_set(ack, ARRAY[$1]::text[], to_jsonb($2::text)) where id=$3`;
+  const values = [clientID, dateStr, id];
+  const sql2 = `SELECT * FROM queue.message where id=$1`;
+  const values2 = [id];
 
   const selectPromise = new Promise(resolve => {
-    pgClient.query(sql, err => {
+    pgClient.query(sql, values, err => {
       if (err) throw Error(`ack update error: ${err}`);
     });
-    pgClient.query(sql2, (err, res) => {
+    pgClient.query(sql2, values2, (err, res) => {
       if (err) throw Error(`ack fetch error: ${err}`);
 
       const date1 = new Date(dateStr);
